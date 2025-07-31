4543121b38ea9d4987b11b67abcc9db28aafadae
Intronium <lucas.laplanche77@gmail.com>
2025-07-31 09:30:39 +0200

    added creator partners

--- _inc/partners.php
+++ _inc/partners.php
@@ -1,11 +1,32 @@
-<section id="partners">
-        <h1>Communautés partenaires</h1>
-        <div class="container flex justify-center align-center">
-            <a href="https://discord.com/invite/G6DEUQ4sBF">
-                <div class="commu-box">
-                    <img src="_img/funfortressfamily.gif" alt="Logo Fun Fortress Family">
-                    <h3>Fun Fortress Family</h3>
-                </div>
-            </a>
+    <section id="partners">
+        <div id="commu-partners">
+            <h1>Communautés partenaires</h1>
+            <div class="container flex justify-center align-center">
+                <a href="https://discord.com/invite/G6DEUQ4sBF">
+                    <div class="commu-box">
+                        <img src="_img/funfortressfamily.gif" alt="Logo Fun Fortress Family">
+                        <h3>Fun Fortress Family</h3>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div id="creator-partners">
+            <h2>Créateurs partenaires</h2>
+            <div class="container flex justify-center">
+                <a href="https://www.youtube.com/@VincentTrodrig">
+                    <div class="creator-box">
+                        <img src="_img/trodrig.jpg" alt="Trodrig">
+                        <h3>Trodrig</h3>
+                        <p>Doubleur Pack VF</p>
+                    </div>
+                </a>
+                <a href="https://www.youtube.com/@aventus66">
+                    <div class="creator-box">
+                        <img src="_img/aventus2.jpg" alt="Aventus">
+                        <h3>Aventus</h3>
+                        <p>Youtuber</p>
+                    </div>
+                </a>
+            </div>
         </div>
     </section>
