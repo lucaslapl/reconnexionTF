b77af2df1ccad53b42590b701ed38004fcf1788c
Intronium <lucas.laplanche77@gmail.com>
2025-08-04 16:44:04 +0200

    moved dbconnect function

--- _src/admin_model.php
+++ _src/admin_model.php
@@ -1,5 +1,30 @@
 <?php 
-include("config.php");
+// Connexion à la DB
+function dbConnect() {
+    error_reporting(E_ALL);
+    $env = parse_ini_file(__DIR__ . '/.env');
+
+    // Détection locale ou prod
+    $isLocal = in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']);
+
+    if ($isLocal) {
+        define("DB_DSN", $env['DB_LOCAL_DSN']);
+        define("DB_USERNAME", $env['DB_LOCAL_USERNAME']);
+        define("DB_PASSWORD", $env['DB_LOCAL_PASSWORD']);
+    } else {
+        define("DB_DSN", $env['DB_PROD_DSN']);
+        define("DB_USERNAME", $env['DB_PROD_USERNAME']);
+        define("DB_PASSWORD", $env['DB_PROD_PASSWORD']);
+    }
+
+    try {
+        $db = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $db;
+    } catch (PDOException $e) {
+        die("Échec lors de la connexion : " . $e->getMessage());
+    }
+}
 
 function adminLogin() {
     $db = dbConnect();
