dce734486da053983e1ffb68e9bb1766e83d8f97
Intronium <lucas.laplanche77@gmail.com>
2025-10-10 10:17:53 +0200

    fixed integrity sha

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ require "_src/news_model.php";
     <?php require_once __DIR__ . '/_templates/footer.php'; ?>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/2f306d349c.js" integrity="sha384-tV2bAJu/9vD0QXTOJWG5kJSnOg7VXobKXr8q75CXDyIrT+wB/vwkMb8ABdmknyUr" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/2f306d349c.js" integrity="sha384-UubVPeRg+bdh8vhqHOVDw9ith9KIv4UkAr/uXAPcetHpb6kcEZvvbQtuWEAmFFu6" crossorigin="anonymous"></script>
     <!-- <script src="_js/jquery-3.3.1.min.js"></script> -->
 	<script src="_js/main.js"></script>
     <script src="_js/streams.js" defer></script>
