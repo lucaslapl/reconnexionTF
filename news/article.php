9d83697531ec02ded9452ef42b15c2b6df1be3d7
Intronium <lucas.laplanche77@gmail.com>
2025-10-14 11:00:06 +0200

    removed fontawesome integrity sha

--- news/article.php
+++ news/article.php
@@ -125,7 +125,7 @@
     <?php require_once '../_templates/footer_sub.php'; ?>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha384-vtXRMe3mGCbOeY7l30aIg8H9p3GdeSe4IFlP6G8JMa7o7lXvnz3GFKzPxzJdPfGK" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/2f306d349c.js" integrity="sha384-UubVPeRg+bdh8vhqHOVDw9ith9KIv4UkAr/uXAPcetHpb6kcEZvvbQtuWEAmFFu6" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/2f306d349c.js" crossorigin="anonymous"></script>
     <!-- <script src="_js/jquery-3.3.1.min.js"></script> -->
 	<script src="_js/news.js"></script>
 </body>
