1eb7a91e575bbf348d226f4a7ab7971ba891e698
Intronium <lucas.laplanche77@gmail.com>
2025-12-04 08:13:58 +0100

    footer update

--- _templates/footer.php
+++ _templates/footer.php
@@ -1,16 +1,12 @@
     <footer>
         <div id="copyright">
-            <div class="container flex justify-center">
-                <div id="recontf-links" class="flex flex-column foot-links">
-                    <h4>reconnexion.tf</h4>
+            <div class="container flex justify-center align-center">
+                <div id="recontf-links" class="flex foot-links">
                     <a href="mailto:contact@reconnexion.tf">Contact</a>
-                    <a href="https://reconnexion.tf/confidentialite.php">Politique de confidentialité</a>
+                    <a href="https://reconnexion.tf/confidentialite.php">Confidentialité</a>
                     <a href="https://statut.reconn.tf/">État des services</a>
-                    <a href="https://beta.reconnexion.tf/news/devenez-partenaire">Programme partenaires</a>
-                    <a href="https://beta.reconnexion.tf/news/rejoignez-l-equipe-reconnexion-tf">Nous rejoindre</a>
                 </div>
-                <div id="social-links" class="flex flex-column foot-links">
-                    <h4>Réseaux</h4>
+                <div id="social-links" class="flex foot-links">
                     <a href="https://discord.gg/ShWmhQDb7H" target="_blank" title="Rejoindre le Discord">
                         Discord
                     </a>
@@ -18,21 +14,8 @@
                         Twitter
                     </a>
                 </div>
-                <div id="mods-links" class="flex flex-column foot-links">
-                    <h4>Mods</h4>
-                    <a href="https://beta.reconnexion.tf/news/telechargez-le-pack-vf-de-tf2">Pack VF TF2</a>
-                </div>
-                <div id="partner-links" class="flex flex-column foot-links">
-                    <h4>Partenaires</h4>
-                    <a href="https://discord.com/invite/G6DEUQ4sBF" target="_blank">Fun Fortress Family</a>
-                </div>
+                <p class="copyright">© Copyright 2025 <a href="https://reconnexion.tf/">reconnexion.tf</a></p>
             </div>
-            <p class="copyright">© Copyright 2025 <a href="https://reconnexion.tf/">reconnexion.tf</a></p>
-        </div>
-
-
-        
-        <div class="foot-bg flex justify-center align-center">
-            <img src="_img/foot-bg.webp" alt="" loading="lazy">
+            
         </div>
     </footer>
