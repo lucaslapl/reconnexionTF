96aed576ac4e7cf3c72c3a6ee5db732d76e5a155
lucas_lapl <lucas.laplanche77@gmail.com>
2025-12-15 15:46:02 +0100

    added fetchpriority high to img

--- _templates/servers.php
+++ _templates/servers.php
@@ -137,7 +137,7 @@
 
         <div class="serveurs-bg flex justify-center align-center">
             <div class="character flex justify-center">
-                <img src="_img/engineer_noel.webp" alt="">
+                <img src="_img/engineer_noel.webp" alt="" fetchpriority="high">
             </div>
             <!--<div class="bg"></div>-->
         </div>
