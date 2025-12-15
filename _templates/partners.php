dab438da06a13b924c6f7a8fd8b2890ba2d154b9
lucas_lapl <lucas.laplanche77@gmail.com>
2025-12-15 07:45:58 +0100

    fff logo change and style adjustments

--- _templates/partners.php
+++ _templates/partners.php
@@ -4,7 +4,7 @@
             <div class="container flex justify-center align-center">
                 <a href="https://discord.com/invite/G6DEUQ4sBF">
                     <div class="commu-box">
-                        <img src="_img/funfortressfamily.gif" alt="Logo Fun Fortress Family">
+                        <img src="_img/Logo_FFF.webp" alt="Logo Fun Fortress Family">
                         <h3>Fun Fortress Family</h3>
                     </div>
                 </a>
