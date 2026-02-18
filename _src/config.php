453c77dcc424385733bcee003a43c2c859bbe9db
Shamp00 <lucas.laplanche77@gmail.com>
2026-02-18 10:10:19 +0100

    test maintenance

--- _src/config.php
+++ _src/config.php
@@ -2,7 +2,7 @@
 // Gestion de maintenance
 // mettre true s'il y a une maintenance à effectuer, sinon laisser false
 
-$maintenance = false;
+$maintenance = true;
 
 if ($maintenance && $_SERVER["REMOTE_ADDR"] !== "37.65.22.206") {
     header('Location: maintenance.php');
