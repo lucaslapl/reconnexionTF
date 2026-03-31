2390759b2f6759afe1cb759642c3e648917c0b23
Shamp00 <lucas.laplanche77@gmail.com>
2026-03-31 14:19:07 +0200

    updated site description

--- _templates/site_desc.php
+++ _templates/site_desc.php
@@ -5,7 +5,7 @@
             <b>reconnexion.tf</b> est une plateforme qui propose des solutions et services à destination des communautés, créateurs de contenu et initiatives ambitieuses francophones sur Team Fortress 2.
         </p>
         <p>
-            Notre objectif est d'accompagner les créateurs dans leurs démarches de développement afin de les mettre en lumière et redynamiser la communauté française.
+            Notre objectif est d'accompagner les créateurs et les porteurs de projets dans leur démarche de développement afin de les mettre en lumière, et redynamiser la communauté française.
         </p>
         <p>
             Si vous êtes un créateur de contenu ou que vous avez une idée de projet à développer en rapport avec Team Fortress 2, n'hésitez pas à nous contacter par mail à l'adresse <a href="mailto:contact@reconnexion.tf">contact@reconnexion.tf</a> ou via notre <a href="https://discord.gg/ShWmhQDb7H">serveur Discord</a> pour en discuter !
